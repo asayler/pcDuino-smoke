3fcf2b44deb444bf0866136c6cf4d6dcba3bdb61
Andy Sayler <andy.sayler@gmail.com>
2014-03-18 16:43:39 -0600

    Removed anlog pin set

--- mq2-alarm.c
+++ mq2-alarm.c
@@ -9,31 +9,30 @@
 #define DIGITAL_PIN_BUTTON  6
 #define ANALOG_PIN_MQ2      0
 
-void playTone(int tone, int duration) {   
-  
+void playTone(int tone, int duration) {
+
   long i = 0;
 
-  for (i = 0; i < duration * 1000L; i += tone * 2) {     
-    digitalWrite(DIGITAL_PIN_SPEAKER, HIGH);     
-    delayMicroseconds(tone);     
-    digitalWrite(DIGITAL_PIN_SPEAKER, LOW);     
-    delayMicroseconds(tone);   
-  } 
-}   
+  for (i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(DIGITAL_PIN_SPEAKER, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(DIGITAL_PIN_SPEAKER, LOW);
+    delayMicroseconds(tone);
+  }
+
+}
 
 void setup() {
 
-  pinMode(ANALOG_PIN_MQ2, INPUT);
-  
-  pinMode((DIGITAL_PIN_SPEAKER + 0), OUTPUT); 
-  pinMode((DIGITAL_PIN_SPEAKER + 1), OUTPUT); 
+  pinMode((DIGITAL_PIN_SPEAKER + 0), OUTPUT);
+  pinMode((DIGITAL_PIN_SPEAKER + 1), OUTPUT);
   digitalWrite((DIGITAL_PIN_SPEAKER + 0), LOW);
   digitalWrite((DIGITAL_PIN_SPEAKER + 1), LOW);
 
-  pinMode((DIGITAL_PIN_BUTTON), INPUT); 
+  pinMode((DIGITAL_PIN_BUTTON), INPUT);
 
 }
- 
+
 void loop() {
 
   int sensorValue = 0;
@@ -41,12 +40,12 @@ void loop() {
 
   printf("silenced = %d\n", silenced);
 
-  sensorValue = analogRead(ANALOG_PIN_MQ2);            
+  sensorValue = analogRead(ANALOG_PIN_MQ2);
   printf("smoke = %d\n", sensorValue);
 
   if(sensorValue > THRESHOLD) {
     printf("ALARM!\n");
-    
+
 
     if (silenced == 0) {
       if (digitalRead(DIGITAL_PIN_BUTTON) == 1) {
@@ -57,7 +56,7 @@ void loop() {
     else {
       delay(100);
     }
-    
+
 
   }
   else {
